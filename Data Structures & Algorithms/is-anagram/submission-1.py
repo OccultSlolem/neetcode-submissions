7c2673e3a9e81ab6c5aa7d508813188ce5ed94e0
Ethan Hanlon <me@ethan-hanlon.xyz>
2026-05-04 17:59:46 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_chars = {}
+        for s_char in s:
+            if s_char not in s_chars: s_chars[s_char] = 0
+            s_chars[s_char] += 1
+        t_chars = {}
+        for t_char in t:
+            if t_char not in t_chars: t_chars[t_char] = 0
+            t_chars[t_char] += 1
+        
+        return s_chars == t_chars
+        
