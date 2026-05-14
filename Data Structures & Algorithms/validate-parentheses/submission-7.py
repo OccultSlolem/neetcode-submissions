6f180534cc9cfb11cd0ebebe0d42f5f9cf5bd222
Ethan Hanlon <me@ethan-hanlon.xyz>
2026-05-13 20:29:06 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        def isProperClose(opening: str, closing: str) -> bool:
+            if opening == "(" and closing == ")": return True
+            elif opening == "{" and closing =="}": return True
+            elif opening == "[" and closing == "]": return True
+            return False
+
+        stack = []
+        opening_chars = ["(", "{", "["]
+        closing_chars = [")", "}", "]"]\
+
+        for char in s:
+            if char in opening_chars:
+                stack.append(char)
+            elif char in closing_chars:
+                if len(stack) == 0 or not isProperClose(stack[-1], char): return False
+                stack.pop()
+        
+        return len(stack) == 0
